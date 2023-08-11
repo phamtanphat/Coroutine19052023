c5c08ced2f80490a144e6bea4bf1944a55ddbfcb
phamtanphat <phatdroid94@gmail.com>
2023-08-11 18:01:27 +0700

    with context

--- app/src/main/java/com/example/coroutine19052023/MainActivity.kt
+++ app/src/main/java/com/example/coroutine19052023/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.coroutine19052023
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineName
@@ -15,6 +16,7 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -28,37 +30,22 @@ class MainActivity : AppCompatActivity() {
         // 3: Job => Life cycle coroutine
         // 4: CoroutineExceptionHandler => Bat loi exception cua coroutine
 
-        val coroutineExceptionHandler = CoroutineExceptionHandler { context, throwable ->
-
-        }
+//        val coroutineExceptionHandler = CoroutineExceptionHandler { context, throwable ->
+//
+//        }
 
         CoroutineScope(
             CoroutineName("Coroutine 1") +
                     Dispatchers.IO +
-                    Job() +
-                    coroutineExceptionHandler
+                    Job()
+//                    coroutineExceptionHandler
         ).launch {
-            supervisorScope {
-                // launch the first child -- its exception is ignored for this example (don't do this in practice!)
-                val firstChild = launch(CoroutineExceptionHandler { _, _ ->  }) {
-                    Log.d("BBB", "First child is failing")
-                    throw AssertionError("First child is cancelled")
-                }
-                // launch the second child
-                val secondChild = launch {
-                    firstChild.join()
-                    // Cancellation of the first child is not propagated to the second child
-                    Log.d("BBB", "First child is cancelled: ${firstChild.isCancelled}, but second one is still active")
-                    try {
-                        delay(Long.MAX_VALUE)
-                    } finally {
-                        // But cancellation of the supervisor is propagated
-                        Log.d("BBB", "Second child is cancelled because supervisor is cancelled")
-                    }
-                }
-                // wait until the first child fails & completes
-                firstChild.join()
-                secondChild.join()
+            for (a in 0..100) {
+                delay(10)
+            }
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Finish", Toast.LENGTH_SHORT).show()
             }
         }
     }
